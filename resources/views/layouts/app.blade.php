2c0a1a754aa8d48a853ea00fe9830bb2d1ae29d9
RiazHossainFahad <mdfahad6562@gmail.com>
2021-02-13 23:17:25 +0600

    loading in app.blade.php for all page and payment/create.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,8 +21,56 @@
 
     @stack('style')
 
+    <style>
+        .centered{
+            width:100%;
+            height:100%;
+            min-width:400px;
+            min-height:400px;
+            position:absolute;
+            top:50%;
+            left:50%;
+            transform:translate(-50%,-50%);
+            background:#000;
+            z-index: 99999;
+            filter: blur(10px) contrast(20);
+        }
+        .blob-1,.blob-2{
+            width:70px;
+            height:70px;
+            position:absolute;
+            background:#fff;
+            border-radius:50%;
+            top:50%;left:50%;
+            transform:translate(-50%,-50%);
+        }
+        .blob-1{
+            left:20%;
+            animation:osc-l 2.5s ease infinite;
+        }
+        .blob-2{
+            left:80%;
+            animation:osc-r 2.5s ease infinite;
+            background:#0ff;
+        }
+        @keyframes osc-l{
+            0%{left:20%;}
+            50%{left:50%;}
+            100%{left:20%;}
+        }
+        @keyframes osc-r{
+            0%{left:80%;}
+            50%{left:50%;}
+            100%{left:80%;}
+        }
+    </style>
+
 </head>
 <body>
+    <div class="centered">
+        <div class="blob-1"></div>
+        <div class="blob-2"></div>
+    </div>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
@@ -108,5 +156,11 @@
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     @stack('script')
+
+    <script>
+        window.onload = () => {
+            $('.centered').css('display', 'none');
+        }
+    </script>
 </body>
 </html>
--- resources/views/payment/create.blade.php
+++ resources/views/payment/create.blade.php
@@ -28,9 +28,56 @@
 
 <!-- link to the custom styles for SqPaymentForm -->
 <link rel="stylesheet" type="text/css" href="/square/form-style.css">
+<style>
+    .centered{
+        width:100%;
+        height:100%;
+        min-width:400px;
+        min-height:400px;
+        position:absolute;
+        top:50%;
+        left:50%;
+        transform:translate(-50%,-50%);
+        background:#000;
+        z-index: 99999;
+        filter: blur(10px) contrast(20);
+    }
+    .blob-1,.blob-2{
+        width:70px;
+        height:70px;
+        position:absolute;
+        background:#fff;
+        border-radius:50%;
+        top:50%;left:50%;
+        transform:translate(-50%,-50%);
+    }
+    .blob-1{
+        left:20%;
+        animation:osc-l 2.5s ease infinite;
+    }
+    .blob-2{
+        left:80%;
+        animation:osc-r 2.5s ease infinite;
+        background:#0ff;
+    }
+    @keyframes osc-l{
+        0%{left:20%;}
+        50%{left:50%;}
+        100%{left:20%;}
+    }
+    @keyframes osc-r{
+        0%{left:80%;}
+        50%{left:50%;}
+        100%{left:80%;}
+    }
+</style>
 
 </head>
 <body>
+    <div class="centered" id="loading">
+        <div class="blob-1"></div>
+        <div class="blob-2"></div>
+    </div>
     <!-- Begin Payment Form -->
     <div class="sq-payment-form">
         
@@ -111,5 +158,10 @@
     </div>
     <!-- End Payment Form -->
 
+    <script>
+        window.onload = () => {
+            document.getElementById('loading').style.display = 'none';
+        }
+    </script>
 </body>
 </html>
